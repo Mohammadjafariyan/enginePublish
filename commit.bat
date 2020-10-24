c539625bec401c979d9e753df2a5875e304271c1
Mohammad jafariyan <mohammad.jafariyan7@gmail.com>
2020-10-24 10:45:31 +0330

    m

---
+++ commit.bat
@@ -0,0 +1,8 @@
+﻿
+
+
+git status & 
+git add . --ignore-removal & git commit -m m & git push https://MohammadJafariyan@21430037@bitbucket.org/MohammadJafariyan/0028-onlinesupportsitepublish.git --all
+
+& pause
+
